3fca9f1037e8d7dbce5a77ae6f5b0dc7390e574e
EmanueleManno <mannus1994@gmail.com>
2023-06-08 14:36:24 +0200

    Creazione della lista che mi stampa gli oggetti

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //PROVO A VEDERE SE FUNZIONA
 console.log('JS');
+
+//PREPARO LA LISTA DEGLI OGGETTI DA COMPRARE:
+const items = ['Pane', 'Focaccia', 'Pizza', 'Piadina', 'Panino']
+
+//STAMPIAMO IN PAGINA:
+let shoppingList = '<ul>';
+
+//GENERO GLI ELEMENTI DELLA LISTA BASANDOMI SUGLI OGGETTI:
+let i = 0;
+while(i < items.length) {
+    shoppingList += `<li>${items[i]}</li>`
+    i++;
+}
+
+//FINE DELLA LISTA:
+shoppingList += '</ul>';
+
+//CARICO NEL BODY:
+document.body.innerHTML = shoppingList;
